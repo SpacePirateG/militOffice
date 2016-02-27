7eb2ab1b045f85c07e7cb87345bebf1a34bc3cce
element2801 <element280105@gmail.com>
2016-02-27 14:35:50 +0300

    add mysql connection, query

--- militOfficeLib/Storage.cs
+++ militOfficeLib/Storage.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace militOfficeLib
 {
     public class Storage
     {
-            public Storage();
+        private string connectSettings;
+        public Storage(string serverName, string userName, string dbName, string port, string password)
+        {
+            connectSettings = "server=" + serverName +
+                ";user=" + userName +
+                ";database=" + dbName +
+                ";port=" + port +
+                ";password=" + password + ";";
+        }
 
+        private void query(string sql)
+        {
+            MySqlConnection connection = new MySqlConnection(connectSettings);
+            MySqlCommand sqlCommand = new MySqlCommand(sql, connection);           
+            DataTable dataTable = new DataTable();
+
+            connection.Open();
+            using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(sqlCommand))
+            {
+                dataAdapter.Fill(dataTable);
+            }
+
+            connection.Close();
+        }
     }
 }

5861895769970d34af4cc3780559c43fcf930d38
Katerina95 <icqkatka@mail.ru>
2016-02-27 19:36:42 +0300

    add class User.cs

---
+++ militOfficeLib/Model/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace militOfficeLib.Model
+{
+    enum Permission
+    {
+        Admin,
+        Chief,
+        AssistChief,
+        Paramedic,
+        TechnicalStaff
+    }
+
+    class User
+    {
+        public String login;
+        public String password;
+        public String name;
+        public String surname;
+        public String patronymic;
+        public Permission permission;
+        public User(
+        String login,
+        String password,
+        String name,
+        String surname,
+        String patronymic,
+        Permission permission
+        )
+        {
+            this.login = login;
+            this.password = password;
+            this.name = name;
+            this.surname = surname;
+            this.patronymic = patronymic;
+        }
+    }
+}

a3979c5e4d31a1f0d54f36e83347ab00cf35f766
Nikola61 <element280105@gmail.com>
2016-02-27 15:24:43 +0300

    add user-admin and commands

---
+++ militOfficeLib/Admin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace militOfficeLib
+{
+    class Admin: Command
+    {
+        Dictionary<string, Command> commands;
+        public Admin()
+        {
+            commands.Add("add_recrut", new AddRecrut());
+            commands.Add("select_recrut", new SelectRecruts());
+            
+        }
+
+        public object execute(object param)
+        {
+            var key = (string)param;
+            var command = commands.First((c) => c.Key == key);
+
+            command.Value.execute(param);
+            return new object();
+        }
+    }
+}

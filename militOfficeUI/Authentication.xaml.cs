3e2f04fbc239c5e9a1ae44915cbc9d30edc89312
SpacePirateG <Vik-dv@yandex.ru>
2016-03-13 00:41:14 +0300

    added militOfficeUI, create Authentication and MainWindow windows

---
+++ militOfficeUI/Authentication.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using militOfficeLib;
+using System.Security.Authentication;
+
+namespace militOfficeUI
+{
+	/// <summary>
+	/// Логика взаимодействия для Authentication.xaml
+	/// </summary>
+	public partial class Authentication : Window
+	{
+		public Authentication()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			MilitTerminal militTerminal = ((App)Application.Current).militTerminal;
+			try
+			{
+				militTerminal.authentication(LoginField.Text, PasswordField.Password);
+				new MainWindow().Show();
+				this.Hide();
+			}
+			catch (AuthenticationException)
+			{
+				MessageBox.Show("Ошибка: Логин или пароль введены неверно");
+			}
+
+				
+		}
+	}
+}

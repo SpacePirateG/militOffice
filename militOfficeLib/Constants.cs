ad39dcd1fdeca6f69da52f32df4d230fd6ccfb5c
SpacePirateG <Vik-dv@yandex.ru>
2016-03-13 19:47:30 +0300

    fixed permissions

--- militOfficeLib/Constants.cs
+++ militOfficeLib/Constants.cs
@@ -14,28 +14,28 @@ namespace militOfficeLib
         public static string port = "19190";
         public static string password = "root";
 
-        public static Commands AdminCommands = Commands.orderRead &
-                                    Commands.orderWrite &
-                                    Commands.recruitsRead &
-                                    Commands.recruitsWrite &
-                                    Commands.userRead &
+        public static Commands AdminCommands = Commands.orderRead |
+                                    Commands.orderWrite |
+                                    Commands.recruitsRead |
+                                    Commands.recruitsWrite |
+                                    Commands.userRead |
                                     Commands.userWrite;
 
-        public static Commands ChiefCommands = Commands.orderRead &
-                                    Commands.orderWrite &
-                                    Commands.recruitsRead &
+        public static Commands ChiefCommands = Commands.orderRead |
+                                    Commands.orderWrite |
+                                    Commands.recruitsRead |
                                     Commands.recruitsWrite;
 
-        public static Commands AssistChiefCommands = Commands.orderRead &
-                                    Commands.orderWrite &
-                                    Commands.recruitsRead &
+        public static Commands AssistChiefCommands = Commands.orderRead |
+                                    Commands.orderWrite |
+                                    Commands.recruitsRead |
                                     Commands.recruitsWrite;
 
-        public static Commands ParamedicCommands = Commands.recruitsRead & Commands.recruitsWrite;
+        public static Commands ParamedicCommands = Commands.recruitsRead | Commands.recruitsWrite;
 
-        public static Commands TechnicalStaffCommands = Commands.orderRead &
-                                    Commands.orderWrite &
-                                    Commands.recruitsRead &
+        public static Commands TechnicalStaffCommands = Commands.orderRead |
+                                    Commands.orderWrite |
+                                    Commands.recruitsRead |
                                     Commands.recruitsWrite;
 
         public static Dictionary<Permission, Commands> availableCommands = new Dictionary<Permission, Commands>{
